38ab4562abb4f688cb06707e1e551866f74d5bf3
Andres-Magic32 <andresl@finerioconnect.com>
2023-02-25 17:14:57 -0600

    add backend

---
+++ backend.tf
@@ -0,0 +1,16 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+# The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
+# For more information, see https://www.terraform.io/docs/backends/types/remote.html
+terraform {
+  backend "remote" {
+    organization = "la-casa-del-tonio"
+
+    workspaces {
+      name = "cloud-iac-with-tonio"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
